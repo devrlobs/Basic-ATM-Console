993b691997a60b5a2ab9bcdc063170ff0af41306
devrlobs <dev.rommellobiano@gmail.com>
2024-02-29 00:28:09 +0800

    Added methods and dummy data

--- ATM-Console-Basic/ATM-Console-Basic/Program.cs
+++ ATM-Console-Basic/ATM-Console-Basic/Program.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 
 
-public class CardHolder
+public class cardHolder
 {
     //declaring variables
     string cardNum;
@@ -12,7 +12,7 @@ public class CardHolder
     
 
     //constructor - is called when an instance of a class is created (used to set initial values)
-    public CardHolder(string cardnum, int pin, string firstName, string lastName)
+    public cardHolder(string cardnum, int pin, string firstName, string lastName, double balance)
     {
         this.cardNum = cardnum;
         this.pin = pin;
@@ -70,6 +70,56 @@ public class CardHolder
 
     public static void Main(string[] args)
     {
+        void printOptions()
+        {
+            Console.WriteLine("Please choose from the following options...");
+            Console.WriteLine("1. Deposit");
+            Console.WriteLine("2. Withdraw");
+            Console.WriteLine("3. Show Balance");
+            Console.WriteLine("4. Exit");
+
+        }
+
+        void deposit(cardHolder currentUser)
+        {
+            Console.WriteLine("How much would you like to deposit: ");
+            double deposit = double.Parse(Console.ReadLine());
+            currentUser.setBalance(deposit);
+            Console.WriteLine("Thank you for depositing, Your new balance is: " + currentUser.getBalance());
+        }
+
+        void withdraw(cardHolder currentUser)
+        {
+            Console.WriteLine("WITHDRAWAL");
+            Console.WriteLine("Amount: ");
+            double withdrawal = double.Parse(Console.ReadLine());
+            if(currentUser.getBalance() > withdrawal)
+            {
+                currentUser.setBalance(currentUser.getBalance() - withdrawal);
+                Console.WriteLine("Funds successfully withdrawn.");
+            }
+            else
+            {
+                Console.WriteLine("Insufficient funds");
+
+            }
+            
+        }
+
+        void balance(cardHolder currentUser)
+        {
+            Console.WriteLine("Account Balance: " + currentUser.getBalance());
+        }
+
+        List<cardHolder> cardHolders = new List<cardHolder>();
+        cardHolders.Add(new cardHolder("123123123123",1234,"Alvin","Grey",123.33));
+        cardHolders.Add(new cardHolder("417722191199",9930,"Richard","Ambrose",332.21));
+        cardHolders.Add(new cardHolder("882991777231",0091,"Galvin","Scott",393.98));
+        cardHolders.Add(new cardHolder("990192777172",0019,"Dawn","Rivero",13.33));
+        cardHolders.Add(new cardHolder("883928776182",3132,"Goldwin","Meyer",120.23));
+        cardHolders.Add(new cardHolder("918287273782",0992,"Rajitha","Malik",1230.33));
+
+
 
     }
 
